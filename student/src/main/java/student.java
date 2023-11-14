e48ceab0036f5a2b61b69682f26d00c1660864a6
abautista2 <144792164+abautista2@users.noreply.github.com>
2023-11-14 17:06:03 +0800

    Updated student.java to have scanner

--- student/src/main/java/student.java
+++ student/src/main/java/student.java
@@ -12,7 +12,16 @@ import java.util.Scanner;
  * @author s1307258
  */
 public class student {
+    personalInformation aPersonalInformation;
+    academicRecord aAcademicRecord;
+    
+    public student(personalInformation aPersonalInformation, academicRecord aAcademicRecord){
+        this.aPersonalInformation = aPersonalInformation;
+        this.aAcademicRecord = aAcademicRecord;
+    }
+    
     public static void main (String[] args){
+        
         //String phoneNum = "";
         //System.out.println(phoneNum.matches("[0-9]+"));
         //String aEmail = "a@asd.cm";
@@ -56,8 +65,64 @@ public class student {
         academicRecord a1 = new academicRecord(80,77,65,92,85);
         academicRecord a2 = new academicRecord(88,95,83,95,100);
         
+        //array to link personalinformation and academicrecord
+        student[] student = new student[2];
+        
+        student[0] = new student(p1,a1);
+        student[1] = new student(p2,a2);
+        
         //
         Scanner sc = new Scanner(System.in);
-        System.out.println("");
+        System.out.println("Enter \"1\" for personal information.\"2\" for academic record.");
+        if(sc.nextInt() == 1){
+            System.out.println("Enter \"1\" to view \"2\" to update");
+            if(sc.nextInt() == 1){
+                if(hkid == student[0].aPersonalInformation.getHkId()){
+                    student[0].aPersonalInformation.viewPersonalInformation();
+                }else{
+                    student[1].aPersonalInformation.viewPersonalInformation();
+                }
+            }else{
+                System.out.println("Which personal information would you like to update?"
+                        + "\nEnter \"1\" for Address \"2\" for Contact Information \"3\" for Emergency Contact Information");
+                if(sc.nextInt() == 1){
+                    System.out.println("What is your new address?");
+                    if(hkid == student[0].aPersonalInformation.getHkId()){
+                        student[0].aPersonalInformation.setAddress(sc.nextLine());
+                    }else{
+                        student[1].aPersonalInformation.setAddress(sc.nextLine());
+                    }
+                }else if(sc.nextInt() == 2){
+                    System.out.println("What is your new emergency contact Information?");
+                    System.out.println("Enter emergency person's name");
+                    String newName = sc.nextLine();
+                    System.out.println("Enter phone number");
+                    String newPhoneNum = sc.nextLine();
+                    if(hkid == student[0].aPersonalInformation.getHkId()){
+                        student[0].aPersonalInformation.setEmergencyContactInfo(new emergencyContactInfo(newName, newPhoneNum));
+                    }else{
+                        student[1].aPersonalInformation.setEmergencyContactInfo(new emergencyContactInfo(newName, newPhoneNum));
+                    }
+                }else{
+                    System.out.println("What is your new contact Information?");
+                    System.out.println("Enter new phone number");
+                    String newPhoneNum = sc.nextLine();
+                    System.out.println("Enter new email");
+                    String newEmail = sc.nextLine();
+                    if(hkid == student[0].aPersonalInformation.getHkId()){
+                        student[0].aPersonalInformation.setContactInfo(new contactInfo(newPhoneNum, newEmail));
+                    }else{
+                        student[1].aPersonalInformation.setContactInfo(new contactInfo(newPhoneNum, newEmail));
+                    }
+                }
+            }
+        }else{
+            if(hkid == student[0].aPersonalInformation.getHkId()){
+                student[0].aAcademicRecord.viewAcademicRecord();
+            }else{
+                student[1].aAcademicRecord.viewAcademicRecord();
+            }
+        }
     }
 }
+
